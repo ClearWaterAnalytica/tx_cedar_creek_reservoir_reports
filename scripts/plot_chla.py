65d68e04988394090192b4159e1127f160effdff
James Watson <watsonjr@users.noreply.github.com>
2022-04-19 10:36:45 -0700

    Update plot_chla.py

--- scripts/plot_chla.py
+++ scripts/plot_chla.py
@@ -118,7 +118,7 @@ vmax = np.percentile(z,95)
 #vmax = 2.25
 
 #! plot points
-im = ax1.scatter(x,y,1,z,marker='s',alpha=.9,vmin=vmin,vmax=vmax,transform=ccrs.PlateCarree(),cmap="jet",edgecolors='none')
+im = ax1.scatter(x,y,1,z,marker='s',alpha=.9,vmin=vmin,vmax=vmax,transform=ccrs.PlateCarree(),cmap="nipy_spectral",edgecolors='none')
 
 #! Colorbar
 cax = fig.add_axes([ax1.get_position().x1+0.01,ax1.get_position().y0,0.02,ax1.get_position().height])
